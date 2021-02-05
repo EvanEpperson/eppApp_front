d79600fd56131d88cec9414f1e7e8f863650474b
Evan Epperson <evanepperson@Evans-MacBook-Pro-2.local>
2021-02-05 10:39:42 -0600

    took node modules out

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { enableScreens } from "react-native-screens"; 
+import "react-native-gesture-handler";
+// import { enableScreens } from "react-native-screens"; 
 import { StyleSheet, Text, View ,Image , SafeAreaView, Button} from 'react-native';
 import { Col, Row, Grid } from "react-native-easy-grid";
 import { vw, vh, vmin, vmax } from "react-native-expo-viewport-units";
-import Sidebar from './Sidebar';
 import ChatBox from './ChatBox';
 
 
@@ -38,14 +38,5 @@ const styles = StyleSheet.create({
     borderWidth: 1,
 
   },
-  container_body: {
-    display: 'flex',
-    flexDirection: 'row',
-    backgroundColor: '#ededed',
-    // marginTop: -50,
-    height: vh(90),
-    width: vw(90),
-    // marginTop: 70,
-  }
 
 });
---
+++ firebase.js
@@ -0,0 +1,12 @@
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB7t1HpPUolF1HWAs7d-6OmxGQnYPvD5Z8",
+    authDomain: "epp-messaging.firebaseapp.com",
+    projectId: "epp-messaging",
+    storageBucket: "epp-messaging.appspot.com",
+    messagingSenderId: "966725075871",
+    appId: "1:966725075871:web:d258154f250ec50b5e73aa",
+    measurementId: "G-3P2L2B760R"
+  };
