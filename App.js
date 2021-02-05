b697fd0a027381fbe20164d19e24d16a8e54f666
Evan Epperson <evanepperson@Evans-MacBook-Pro-2.local>
2021-02-04 22:10:19 -0600

    saving for the night about to start on screens

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { enableScreens } from "react-native-screens"; 
+import { StyleSheet, Text, View ,Image , SafeAreaView, Button} from 'react-native';
 import { Col, Row, Grid } from "react-native-easy-grid";
 import { vw, vh, vmin, vmax } from "react-native-expo-viewport-units";
 import Sidebar from './Sidebar';
 import ChatBox from './ChatBox';
 
+
+
 export default function App() {
+  const testing = () => {
+    console.log("hello");
+  }
+
+
+
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.container_body}>
         <Sidebar />
         <ChatBox />
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -32,10 +42,10 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     backgroundColor: '#ededed',
-    marginTop: -50,
+    // marginTop: -50,
     height: vh(90),
     width: vw(90),
-    marginTop: 70,
+    // marginTop: 70,
   }
 
 });
